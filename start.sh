1fbb6efbb24eda2296caf03d6211d3c76a3c9970
asiwww <asiwww@tramwaj.asi.pwr.wroc.pl>
2013-03-03 13:30:57 +0100

    Zmieniona lokalizacja rvm-a w skrypcie startowym

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /etc/profile
 export LANG=en_US.UTF-8
-source "/home/redmine/.rvm/scripts/rvm" || exit 1
+source "/home/asiwww/.rvm/scripts/rvm" || exit 1
 cd `dirname $0`
 rvm use 1.9.3
 thin start -s1 -p3000
